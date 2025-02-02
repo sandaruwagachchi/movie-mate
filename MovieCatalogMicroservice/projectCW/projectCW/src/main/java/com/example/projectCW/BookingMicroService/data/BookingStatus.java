215bb5e1891d1bd431a6d62a8020d9ddbef10297
Thenula09 <thenulahansaja12@gmail.com>
2025-02-02 23:56:08 +0530

    Delete MovieCatalogMicroservice/projectCW directory

--- MovieCatalogMicroservice/projectCW/projectCW/src/main/java/com/example/projectCW/BookingMicroService/data/BookingStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.projectCW.BookingMicroService.data;
-
-public enum BookingStatus {
-    AVAILABLE, BOOKED
-}
--- MovieCatalogMicroservice/projectCW/projectCW/src/main/java/com/example/projectCW/LoginMicroservice/data/registerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.projectCW.LoginMicroservice.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface registerRepository extends JpaRepository<register, Long> {
-}
